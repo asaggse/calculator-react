ad64acbe3349e0a1992eca7d01de88e4395a6e12
Alessandro Saggese <asaggese@gmail.com>
2023-01-27 20:05:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,29 +53,34 @@ function App() {
   }
 
   return (
-    <div className="calculator">
-      <div className="display">{value}</div>
-      <div className="numbers">
-        <button onClick={() => handleNumberClick(1)}>1</button>
-        <button onClick={() => handleNumberClick(2)}>2</button>
-        <button onClick={() => handleNumberClick(3)}>3</button>
-        <button onClick={() => handleNumberClick(4)}>4</button>
-        <button onClick={() => handleNumberClick(5)}>5</button>
-        <button onClick={() => handleNumberClick(6)}>6</button>
-        <button onClick={() => handleNumberClick(7)}>7</button>
-        <button onClick={() => handleNumberClick(8)}>8</button>
-        <button onClick={() => handleNumberClick(9)}>9</button>
-        <button onClick={() => handleNumberClick(0)}>0</button>
-        <button onClick={() => setValue('')}>C</button>
+    <>
+      <div style={{ textAlign: "center" }}>
+        <h1>Calculator</h1>
       </div>
-      <div className="operators">
-        <button onClick={() => handleOperatorClick('+')}>+</button>
-        <button onClick={() => handleOperatorClick('-')}>-</button>
-        <button onClick={() => handleOperatorClick('*')}>x</button>
-        <button onClick={() => handleOperatorClick('/')}>/</button>
-        <button onClick={handleEqualClick}>=</button>
+      <div className="calculator">
+        <div className="display">{value}</div>
+        <div className="numbers">
+          <button onClick={() => handleNumberClick(1)}>1</button>
+          <button onClick={() => handleNumberClick(2)}>2</button>
+          <button onClick={() => handleNumberClick(3)}>3</button>
+          <button onClick={() => handleNumberClick(4)}>4</button>
+          <button onClick={() => handleNumberClick(5)}>5</button>
+          <button onClick={() => handleNumberClick(6)}>6</button>
+          <button onClick={() => handleNumberClick(7)}>7</button>
+          <button onClick={() => handleNumberClick(8)}>8</button>
+          <button onClick={() => handleNumberClick(9)}>9</button>
+          <button onClick={() => handleNumberClick(0)}>0</button>
+          <button onClick={() => setValue('')}>C</button>
+        </div>
+        <div className="operators">
+          <button onClick={() => handleOperatorClick('+')}>+</button>
+          <button onClick={() => handleOperatorClick('-')}>-</button>
+          <button onClick={() => handleOperatorClick('*')}>x</button>
+          <button onClick={() => handleOperatorClick('/')}>/</button>
+          <button onClick={handleEqualClick}>=</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
